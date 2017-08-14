73ad09b6fda5ba3c60a85ccd0e17971bfced2dc9
wojciech.slupski <wojciech.slupski@apptrend.pl>
2017-08-14 20:56:11 +0200

    dodanie content

--- FitView/src/app/app.module.ts
+++ FitView/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
     HeaderComponent,
+    ContentComponent,
     FooterComponent
   ],
   imports: [
